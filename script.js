f20bad93235d202e47f481716f8e5da69e3cee30
kevdela <kevin.delattre@datakhi.com>
2023-03-30 14:16:43 +0200

    feat: add Datakhi description

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 //////////////// HEADER ////////////////
 const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry)=>{
+    entries.forEach((entry) => {
         console.log(entry)
-        if (entry.isIntersecting){
+        if (entry.isIntersecting) {
             entry.target.classList.toggle('show', entry.isIntersecting);
         } else {
             entry.target.classList.remove('show');
@@ -19,21 +19,21 @@ const nav = document.querySelector('nav');
 
 
 
-window.addEventListener('scroll',() =>{
+window.addEventListener('scroll', () => {
     let getscroll = window.scrollY;
     console.log(getscroll);
-    switch (true){
-        case (getscroll >= 905) && (getscroll <= 1699) :
+    switch (true) {
+        case (getscroll >= 905) && (getscroll <= 1699):
             nav.classList.add('active_nav_1');
             nav.classList.remove('active_nav_2');
             nav.classList.remove('active_nav_3');
             break;
-        case (getscroll >= 1956) && (getscroll <= 2899 ):
+        case (getscroll >= 1956) && (getscroll <= 2899):
             nav.classList.add('active_nav_2');
             nav.classList.remove('active_nav_1');
             nav.classList.remove('active_nav_3');
             break;
-        case (getscroll >= 1900 ):
+        case (getscroll >= 1900):
             nav.classList.add('active_nav_3');
             nav.classList.remove('active_nav_1');
             nav.classList.remove('active_nav_2');
@@ -53,6 +53,7 @@ let buttonPuissance4 = document.getElementById("button-puissance4");
 let buttonBeepsong = document.getElementById("button-beepsong");
 let buttonPiano = document.getElementById("button-virtualPiano");
 let buttonDevis = document.getElementById("button-Devis");
+let buttonId = document.getElementById("button-Idkids")
 let buttonCV = document.getElementById("button-CV");
 let close = document.getElementsByClassName("close-window");
 
@@ -62,12 +63,13 @@ let popup2 = document.getElementById("project2-popup");
 let popup3 = document.getElementById("project3-popup");
 let popup4 = document.getElementById("project4-popup");
 let popup5 = document.getElementById("project5-popup");
+let popup6 = document.getElementById("project6-popup");
 let blur = document.getElementById("blur");
 let scroll = document.getElementById("stopScroll");
 
 
 //Function
-function toggleBlur(){
+function toggleBlur() {
     blur.classList.toggle("active");
     scroll.classList.toggle("active");
 }
@@ -79,40 +81,40 @@ function hidePopup() {
     popup3.classList.remove("active");
     popup4.classList.remove("active");
     popup5.classList.remove("active");
+    popup6.classList.remove("active");
 }
 
 //onclick trigger
-buttonPuissance4.addEventListener("click",() => {
+buttonPuissance4.addEventListener("click", () => {
     toggleBlur();
     popup1.classList.toggle("active");
 });
-buttonBeepsong.addEventListener("click",() =>{
+buttonBeepsong.addEventListener("click", () => {
     toggleBlur();
     popup2.classList.toggle("active");
 });
 
-buttonPiano.addEventListener("click",() =>{
+buttonPiano.addEventListener("click", () => {
     toggleBlur();
     popup3.classList.toggle("active");
 });
 
-buttonDevis.addEventListener("click",() =>{
+buttonDevis.addEventListener("click", () => {
     toggleBlur();
     popup4.classList.toggle("active");
 });
 
-buttonCV.addEventListener("click",() =>{
+buttonId.addEventListener("click", () => {
     toggleBlur();
     popup5.classList.toggle("active");
 });
+buttonCV.addEventListener("click", () => {
+    toggleBlur();
+    popup6.classList.toggle("active");
+});
 
 Array.from(close).forEach(function(element) {
-    element.addEventListener('click', () =>{
+    element.addEventListener('click', () => {
         hidePopup();
     });
 });
-
-
-
-
-
